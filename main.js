53451920a1c2f1f2a555ffeb6b17f3931a2933e4
FraaQuartu <francescoquartu50@gmail.com>
2024-04-15 23:08:46 +0200

    Implement the event listener

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
-const gridSize = 16; 
-const gridPixelWidth = 960;
-const divWidth = String(gridPixelWidth / gridSize) + 'px';
-const divHeight = divWidth;
+// Add row to a container
+const addRow = function(container) {
+  let newRow = document.createElement("div");
+  newRow.style.display = "flex";
+  newRow.style.flex = "1";
+  container.appendChild(newRow);
+  return newRow;
+}
 
-container = document.querySelector("#container");
+// Add a square in a row
+const addSquare = function(row) {
+  let newDiv = document.createElement("div");
+  newDiv.style.flex = "1";
+  newDiv.style.border = "1px solid black";
+  newDiv.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "black";
+  });
+  row.appendChild(newDiv);
+  return newDiv;
+}
 
+
+const gridSize = 32; 
+let container = document.querySelector("#container");
 let newRow;
 
 for(let i = 0; i < gridSize; i++) {
-  newRow = document.createElement("div");
-  newRow.style.display = "flex";
-  newRow.style.flex = "1";
+  newRow = addRow(container);
   for(let j = 0; j < gridSize; j++) {
-    // Aggiungi il div
-    newDiv = document.createElement("div");
-    newDiv.style.flex = "1";
-    newDiv.style.border = "1px solid black";
-
-    newRow.appendChild(newDiv);
+    addSquare(newRow);
   }
-  container.appendChild(newRow)
 }
