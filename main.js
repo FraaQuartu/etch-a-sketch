98b8baafdbad660dc2cda841f46926f48766b4df
FraaQuartu <francescoquartu50@gmail.com>
2024-04-15 23:25:19 +0200

    Make grid size variable

--- main.js
+++ main.js
@@ -19,14 +19,35 @@ const addSquare = function(row) {
   return newDiv;
 }
 
-
-const gridSize = 32; 
-let container = document.querySelector("#container");
-let newRow;
-
-for(let i = 0; i < gridSize; i++) {
-  newRow = addRow(container);
-  for(let j = 0; j < gridSize; j++) {
-    addSquare(newRow);
+const displayGrid = function(gridSize) {
+  let container = document.querySelector("#container");
+  let newRow;
+  for(let i = 0; i < gridSize; i++) {
+    newRow = addRow(container);
+    for(let j = 0; j < gridSize; j++) {
+      addSquare(newRow);
+    }
   }
 }
+
+
+/* ------------- Main script ----------- */
+let gridSize = 32; 
+displayGrid(gridSize);
+
+button = document.querySelector("button");
+button.addEventListener("click", () => {
+  // Prompt
+  gridSize = prompt("Enter new grid size");
+
+  // Delete the grid
+  let rows = document.querySelectorAll("#container > div");
+  rows.forEach((row) =>{
+    row.remove();
+  });
+
+  // Display the new grid
+  displayGrid(gridSize);
+})
+
+
